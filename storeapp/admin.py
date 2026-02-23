58a0caa5b56978b2005919f3e1ba0aa0cd459ee9
udehmxinrrr <udehmxinrrr@gmail.com>
2026-02-23 09:49:17 +0300

    Updated SavannahStore

--- storeapp/admin.py
+++ storeapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from storeapp.models import Product
+
 # Register your models here.
+admin.site.register(Product)
--- storeapp/models.py
+++ storeapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    ProductName = models.CharField(max_length=100)
+    Quantity = models.IntegerField()
+    Price = models.FloatField()
+    Description = models.TextField()
+
+    def __str__(self):
+        return self.ProductName
+
+
